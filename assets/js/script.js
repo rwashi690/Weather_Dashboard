4a7c4e5bbaf4f6610da157e34ec8c07e80234fe9
rwashi690 <rwashington325@gmail.com>
2022-01-11 18:54:14 -0700

    Created a fetch function to convert the city to lattitude and longitude

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,3 +15,21 @@ function weatherRequest(latitude, longitude) {
 		});
 }
 
+// Creating a fetch request 
+function getCity (cityRequested){
+	fetch("http://api.openweathermap.org/geo/1.0/direct?q="+cityRequested+"&limit=1&appid=00e50f3e30c93092b592e69d5a4564df", {
+		"method": "GET"
+		}
+	)
+		.then(response => {
+			return response.json();
+		})
+		.then(data => {
+			console.log(data);
+		})
+		.catch(err => {
+			console.error(err);
+		});    
+}
+
+console.log(getCity('Dallas'))
