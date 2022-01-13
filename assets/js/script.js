d51d016ba7ac4f9835697d34ac0b065176cb7b03
rwashi690 <rwashington325@gmail.com>
2022-01-12 23:29:58 -0700

    Defined necessary HTML elements in JS file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,11 @@
-// Creating a function to determine the weather at a specific lattitude and longitude
+// Define necessary variables to target variables 
+submitBtn = document.getElementById('weatherSubmit');
+searchInput = document.getElementById('weatherInput');
+currentWeatherEl = document.getElementById('currentWeather');
+fiveDayWeatherEl =document.getElementById('5dayWeather');
+pastSearches =document.getElementById('searchList');
+
+// Creating a fetch request to determine the weather at a specific lattitude and longitude
 function weatherRequest(latitude, longitude) {
 	fetch("https://api.openweathermap.org/data/2.5/onecall?lat="+latitude+"&lon="+longitude+"&exclude=minutely,hourly&appid=00e50f3e30c93092b592e69d5a4564df", {
 		"method": "GET"
@@ -15,7 +22,7 @@ function weatherRequest(latitude, longitude) {
 		});
 }
 
-// Creating a fetch request 
+// Creating a fetch request to obtain the weather from the listed city
 function getCity (cityRequested){
 	fetch("http://api.openweathermap.org/geo/1.0/direct?q="+cityRequested+"&limit=1&appid=00e50f3e30c93092b592e69d5a4564df", {
 		"method": "GET"
@@ -34,4 +41,4 @@ function getCity (cityRequested){
 		});    
 }
 
-// console.log(getCity('Dallas'))
+console.log(getCity('Dallas'))
